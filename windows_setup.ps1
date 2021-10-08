b202fbbefa09b709efe3e5c4e008fcc7a8997ba3
Motohiro <c201075@gmail.com>
2021-10-08 09:32:52 +0900

    Update windows_setup.ps1
    update

--- windows_setup.ps1
+++ windows_setup.ps1
@@ -1,9 +1,12 @@
 # witout option
-choco install -y googlechrome adobereader direcx silverlight slack sourcetree mobaxterm 7zip.install microsoft-teams vcredist140 arduino chocolateygui zoom
-choco install -y powertoys
+choco install -y googlechrome adobereader silverlight slack sourcetree mobaxterm 7zip.install microsoft-teams vcredist140 chocolateygui zoom
+choco install -y powertoys mousewithoutborders 
+
+# proprietary 
+choco install git-fork groupy
 
 # with option
-choco install python3 --version=3.7.8
+choco install python3 --version=3.8.10
 choco install vscode -y --params "/NoContextMenuFiles /NoContextMenuFolders"
 choco install git -y --params "/WindowsTerminal /NoShellIntegration /NoGuiHereIntegration /NoShellHereIntegration /SChannel /NoAutoCrlf"
 choco install pdfxchangeeditor -y -params '"/NoDesktopShortcuts /NoUpdate /NoViewInBrowsers /NoSetAsDefault"'
